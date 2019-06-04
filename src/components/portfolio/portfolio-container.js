18cf265e3969bbcdb28a65c5b9a7a2f35c1159a4
Jason Hall <jhallnation@gmail.com>
2019-06-04 14:23:59 -0600

    portfolio container and item components

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import PortfolioItem from './portfolio-item';
+
+export default class PortfolioContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Portfolio Items</h2>
+
+        <PortfolioItem />
+      </div>
+    );
+  }
+}
