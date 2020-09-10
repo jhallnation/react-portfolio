3883ca2ae4df74af725c95ea64a06804c2511187
Jason Hall <jhallnation@gmail.com>
2020-09-10 12:03:01 -0600

    get porfolio for detail page

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default function(props) {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  )
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      portfolioItemID: this.props.match.params.slug,
+      portfolioItem: {}
+    }
+
+    this.getPortfolioItem = this.getPortfolioItem.bind(this);
+  }
+
+   getPortfolioItem() {
+    axios
+      .get('http://localhost:3000/api/portfolio/item',
+        { 
+          headers: { 
+            'portfolioItemID' : this.state.portfolioItemID
+          }
+        })
+      .then(response => {
+        this.setState({
+          portfolioItem: response.data,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+    });
+  }
+
+  componentDidMount(){
+    this.getPortfolioItem();
+  }
+
+  render() {
+    const { title, subtitle, body, main_image, thumb_image, logo, work_type, url } = this.state.portfolioItem;
+
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+        <h1>{title}</h1>
+      </div>
+    )
+  }
 }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -9,12 +9,6 @@ export default class PortfolioItem extends Component {
       portfolioItemClass: ''
     };
   }
-      // Data tgat I'll need
-    // - background image: thumb_image_url
-    // - logo: logo_url
-    // - id: id
-    // - url: url NOT in schema, needs to be added
-    // ["id", "name", "description", "url", "category", "position", "thumb_image_url", "banner_image_url", "logo_url", "column_names_merged_with_images"]
 
   handleMouseEnter(){
     this.setState({portfolioItemClass: 'image-blur'});
@@ -27,26 +21,25 @@ export default class PortfolioItem extends Component {
   render(){
     const { title, subtitle, body, thumb_image, id } = this.props.item;
     return (
-      <div 
-        className='portfolio-item-wrapper '
-        onMouseEnter={() => this.handleMouseEnter()}
-        onMouseLeave={() => this.handleMouseLeave()}
-       >
-        <div
-          className={'portfolio-img-background ' + this.state.portfolioItemClass}
-          style={{
-            backgroundImage: 'url(' + thumb_image.url + ')'
-          }}
-        />
+      <Link to={`/portfolio/${id}`}>
+        <div 
+          className='portfolio-item-wrapper '
+          onMouseEnter={() => this.handleMouseEnter()}
+          onMouseLeave={() => this.handleMouseLeave()}
+         >
+          <div
+            className={'portfolio-img-background ' + this.state.portfolioItemClass}
+            style={{
+              backgroundImage: 'url(' + thumb_image.url + ')'
+            }}
+          />
 
-        <div className='img-text-wrapper'>
-          <div className='title-wrapper'>{title}</div>
-          <div className='subtitle'>{subtitle}</div>
+          <div className='img-text-wrapper'>
+            <div className='title-wrapper'>{title}</div>
+            <div className='subtitle'>{subtitle}</div>
+          </div>
         </div>
-        {/* <h3>{url}</h3> not currently in portfolio table */}
-        {/* <Link to={`/portfolio/${id}`}>Link</Link> */}
-       
-      </div>
+      </Link> 
     )
   }
 }
